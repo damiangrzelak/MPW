c77db5538e1e7d29d17c493c3108cfbf4373404c
damiangrzelak <damiangrzelak93@gmail.com>
2019-03-12 21:10:51 +0100

    Add projects

---
+++ ConsoleApp/ClientApp/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    class Client
+    {
+        private static String clientName;
+        private static String pathToLocalDirectory;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client");
+            if (args.Length == 2)
+            {
+                clientName = args[0];
+                pathToLocalDirectory = args[1];
+
+                Console.WriteLine("User {0} localDir = {1}", clientName, pathToLocalDirectory);
+
+            }
+        }
+    }
+}
