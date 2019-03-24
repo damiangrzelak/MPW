41b3a64c98b8679a44a88c2badc57a33b46df30a
Damian Grzelak <damiangrzelak93@gmail.com>
2019-03-24 17:10:16 +0100

    Simple tcp client-server communication

--- ConsoleApp/ClientApp/Client.cs
+++ ConsoleApp/ClientApp/Client.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +13,9 @@ namespace ClientApp
     {
         private static String clientName;
         private static String pathToLocalDirectory = @"d:\DropboxApp\ClientSpace\";
+
+        private static readonly int port = 1234;
+        private static readonly String ipAdress= "127.0.0.1";
 
         static void Main(string[] args)
         {
@@ -25,7 +30,34 @@ namespace ClientApp
                 if (Directory.Exists(pathToLocalDirectory))
                 {
                     Console.WriteLine("Directory for {0}", clientName);
-                    CheckIfNewFileIsOnServer();
+                    try
+                    {
+                        TcpClient client = new TcpClient(ipAdress, port);
+                        StreamReader reader = new StreamReader(client.GetStream());
+                        StreamWriter writer = new StreamWriter(client.GetStream());
+                        writer.WriteLine(clientName);
+                        writer.Flush();
+                        String s = String.Empty;
+                        while (!s.Equals("Exit"))
+                        {
+                            Console.Write("Enter a string to send to the server: ");
+                            s = Console.ReadLine();
+                            Console.WriteLine();
+                            writer.WriteLine(s);
+                            writer.Flush();
+                            String server_string = reader.ReadLine();
+                            Console.WriteLine(server_string);
+                            //CheckIfNewFileIsOnServer();
+
+                        }
+                        reader.Close();
+                        writer.Close();
+                        client.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
 
                 }
                 else
--- ConsoleApp/ServerApp/Server.cs
+++ ConsoleApp/ServerApp/Server.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ServerApp
@@ -20,11 +23,15 @@ namespace ServerApp
 
         };
 
+        private static readonly int port = 1234;
+        private static readonly IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Server");
 
-            foreach(String disk in diskSpaceList)
+            foreach (String disk in diskSpaceList)
             {
                 String path = pathToLocalDirectory + disk;
                 if (!Directory.Exists(path))
@@ -33,7 +40,74 @@ namespace ServerApp
                     Directory.CreateDirectory(path);
                 }
             }
+            StartServer();
 
+
+        }
+
+        private static void StartServer()
+        {
+            TcpListener tcpListener = null;
+            try
+            {
+                tcpListener = new TcpListener(localAddr, port);
+                tcpListener.Start();
+                Console.WriteLine("Server is started... {0}::{1}", localAddr, port);
+                for(; ; )
+                {
+                    Console.WriteLine("Waiting for connections...");
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                    Thread thread = new Thread(ProcessClient);
+                    thread.Start(tcpClient);
+                }
+            }
+            catch(Exception err)
+            {
+                Console.WriteLine(err);
+            }
+            finally
+            {
+                if (tcpListener != null)
+                    tcpListener.Stop();
+            }
+        }
+
+        private static void ProcessClient(object tcpClient)
+        {
+            TcpClient client = (TcpClient)tcpClient;
+            try
+            {
+                StreamReader reader = new StreamReader(client.GetStream());
+                StreamWriter writer = new StreamWriter(client.GetStream());
+                String s = String.Empty;
+                String clientName = String.Empty;
+                clientName = reader.ReadLine();
+                Console.WriteLine("New connection from {0} ...", clientName);
+
+
+                while (!(s = reader.ReadLine()).Equals("Exit") || (s == null))
+                {
+                    Console.WriteLine("From client -> " + s);
+                    writer.WriteLine("From server -> " + s);
+                    writer.Flush();
+                }
+                
+                reader.Close();
+                writer.Close();
+                client.Close();
+                Console.WriteLine("Closing client connection!");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Problem with client communication. Exiting thread.");
+            }
+            finally
+            {
+                if (client != null)
+                {
+                    client.Close();
+                }
+            }
         }
     }
 }
